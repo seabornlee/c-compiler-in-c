dcbef0eb15a56b87baa2e79558a59e1459d62a43
Seaborn Lee <seabornlee.cn@gmail.com>
2020-01-28 06:47:17 +0800

    feat: parse all c keywords

--- test/scanner_test.cpp
+++ test/scanner_test.cpp
@@ -9,9 +9,24 @@ TEST(ScannerTest, getNextTokenShouldReturnEndWhenEnded) {
     EXPECT_EQ(END, token->type);
 }
 
-TEST(ScannerTest, getNextTokenShouldReturnKeywordToken) {
-    Scanner scanner("int");
+class KeywordParameterizedTestFixture : public ::testing::TestWithParam<string> {};
+
+TEST_P(KeywordParameterizedTestFixture, getNextTokenShouldReturnKeywordToken) {
+    ParamType keyword = GetParam();
+    Scanner scanner(keyword);
     Token *token = scanner.getNextToken();
     EXPECT_EQ(KEYWORD, token->type);
-    EXPECT_EQ("int", token->name);
+    EXPECT_EQ(keyword, token->name);
 }
+
+INSTANTIATE_TEST_SUITE_P
+(
+        ScannerTest,
+        KeywordParameterizedTestFixture,
+        ::testing::Values(
+                "int", "auto", "break", "case",	"char", "const", "continue", "default", "do",
+                "double", "else", "enum", "extern", "float", "for", "goto", "if",
+                "int", "long", "register", "return", "short", "signed", "sizeof", "static",
+                "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
+        )
+);
